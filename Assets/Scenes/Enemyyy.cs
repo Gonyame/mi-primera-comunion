13213614a5b568bd07ee44e884aae176dff28b54
Akko004 <a22390170@ucslp.net>
2023-04-19 23:03:28 -0600

    menu

---
+++ Assets/Scenes/Enemyyy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyyy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5f;
+    public float detectionDistance = 5f;
+    public float stoppingDistance = 1f;
+    private bool isFollowing = false;
+
+    void Update()
+    {
+        float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+        if (distanceToTarget <= detectionDistance && distanceToTarget > stoppingDistance)
+        {
+            isFollowing = true;
+        }
+        else
+        {
+            isFollowing = false;
+        }
+
+        if (isFollowing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}

63ed4a02a4c3b363404cc55a0905a56c85d53fda
Alejandro Medina <drolejan@gmail.com>
2023-05-04 09:16:40 -0600

    ya arregle su trampa, no leas esto Jaz

--- Assets/scriBugueado/playerHealt.cs
+++ Assets/scriBugueado/playerHealt.cs
@@ -15,22 +15,5 @@ public class playerHealt : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("oso"))
-        {
-            PlayerController.instance.atrapado();
-            transform.position = collision.transform.position + new Vector3(0f, 0f, 0f);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("oso"))
-        {
-            PlayerController.instance.atrapado();
-            transform.position = collision.transform.position + new Vector3(0f, 0f, 0f);
-        }
-    }
 }
 
